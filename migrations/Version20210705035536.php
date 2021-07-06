c58f12298898687e8f14f4eaad47784954ff2178
RaduGitHub <radu.mihai17@yahoo.com>
2021-07-06 04:10:30 -0700

    database

--- migrations/Version20210705035536.php
+++ migrations/Version20210705035536.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210705030205 extends AbstractMigration
+final class Version20210705035536 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,13 @@ final class Version20210705030205 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE license_plate (id INT AUTO_INCREMENT NOT NULL, user_id_id INT DEFAULT NULL, license_plate VARCHAR(10) NOT NULL, INDEX IDX_F5AA79D09D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE license_plate (id INT AUTO_INCREMENT NOT NULL, license_plate VARCHAR(10) NOT NULL, user_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(320) NOT NULL, password VARCHAR(60) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE license_plate ADD CONSTRAINT FK_F5AA79D09D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE license_plate DROP FOREIGN KEY FK_F5AA79D09D86650F');
         $this->addSql('DROP TABLE license_plate');
         $this->addSql('DROP TABLE user');
     }
---
+++ src/Entity/Activity.php
@@ -0,0 +1,91 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Activity
+ *
+ * @ORM\Table(name="activity")
+ * @ORM\Entity
+ */
+class Activity
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="blocker", type="string", length=10, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $blocker;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="blockee", type="string", length=10, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $blockee;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer", nullable=false)
+     */
+    private $status = '0';
+
+    /**
+     * @return string
+     */
+    public function getBlocker(): string
+    {
+        return $this->blocker;
+    }
+
+    /**
+     * @param string $blocker
+     */
+    public function setBlocker(string $blocker): Activity
+    {
+        $this->blocker = $blocker;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockee(): string
+    {
+        return $this->blockee;
+    }
+
+    /**
+     * @param string $blockee
+     */
+    public function setBlockee(string $blockee): Activity
+    {
+        $this->blockee = $blockee;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus($status): Activity
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+}
